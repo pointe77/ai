273b9235f3513e16c15e67312fef9b16f4e18982
Ryan Lee <strike77@gmail.com>
2019-02-26 23:26:27 -0800

    stock price prediciton

---
+++ rabbitmq/new_task.py
@@ -0,0 +1,11 @@
+import sys 
+import pika
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+channel = connection.channel()
+
+message = ' '.join(sys.argv[1:]) or "Hello World!"
+
+channel.basic_publish(exchange='', routing_key='hello', body=message)
+
+print(" [x] Sent %r" % message)
